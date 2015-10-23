e8566b40735248836a3470ebfa2b9ae4f12a156c
rodolfosrg <rodolfosrg@gmail.com>
2015-10-22 22:46:03 -0600

    Main loop and basic file read.

--- the_raven.py
+++ the_raven.py
@@ -1,2 +1,11 @@
+import os
+os.system('cls')
 with open('a_raven.ascii', 'r') as f:
 	print f.read()
+
+raw_input("Press ENTER to continue...")
+os.system('cls')
+for x in range(1,2): #Change this to (1,21) in final version
+	print "Loading problem %02d" % (x,)
+	with open('Problems/2x1BasicProblem%02d.txt' % (x,), 'r') as p:
+		print p.read()
